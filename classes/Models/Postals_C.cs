d7636ec9930cef6527eebedc3c33848c9c7e3a4f
Arnoldguti <arnolr.gutierrez@outlook.com>
2017-08-01 16:56:18 +0200

    Add files via upload

---
+++ classes/Models/Postals_C.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Common;
+
+namespace Project.Models
+{
+    public class Postals_C : BindableBase
+    {
+
+        private string _pathCode;
+
+        public string PathCode
+        {
+            get { return _pathCode; }
+            set { SetProperty(ref _pathCode, value); }
+        }
+
+
+        private string _pathName;
+
+        public string PathName
+        {
+            get { return _pathName; }
+            set { SetProperty(ref _pathName, value); }
+        }
+
+     
+        
+        private string _pathDate;
+
+        public string PathDate
+        {
+            get { return _pathDate; }
+            set { SetProperty(ref _pathDate, value); }
+        }
+        
+    }
+}
